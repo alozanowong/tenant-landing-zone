3cc480000918579c3b93d61aa179688de29c4ec9
alozanowong <alejlw@gmail.com>
2026-06-14 13:16:52 -0400

    fix: resolve variable duplication between main and locals

--- modules/governance/main.tf
+++ modules/governance/main.tf
@@ -9,6 +9,21 @@
 #   5. Diagnostic settings forwarding MG activity to Log Analytics
 ###############################################################################
 
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.90.0, < 4.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.47.0, < 3.0.0"
+    }
+  }
+}
+
+
 ###############################################################################
 # MANAGEMENT GROUP
 ###############################################################################
